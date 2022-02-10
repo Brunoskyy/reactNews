57c1441857a14b0d58803006d088b723a0139b8f
Brunoskyy <arturbrunoferreira@gmail.com>
2022-02-10 12:43:12 -0300

    Validate active subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,39 +1,48 @@
-import { getSession, signIn } from 'next-auth/react';
+import { getSession, signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
-
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const session = getSession();
+  const { data: session} = useSession();
+  console.log(session);
 
-  async function handleSubmit() {
-    if(!session) {
-      signIn("github");
+  const router = useRouter();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
     try {
       const response = await api.post('/subscribe')
 
-      const { sessionId } = response.data
+      const { sessionId } = response.data;
 
       const stripe = await getStripeJs();
 
-      await stripe.redirectToCheckout({ sessionId });
-    } catch(err) {
-      alert(err.message)
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message);
     }
-  };
+  }
+
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
-      onClick={handleSubmit}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
